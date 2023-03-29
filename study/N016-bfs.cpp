4f3ebc6c9a7a47cfbc02bc4dda5ae9440f278f43
ysystudio <ysystudio@gmail.com>
2023-03-29 21:00:07 +0800

    update N016-bfs

--- study/N016-bfs.cpp
+++ study/N016-bfs.cpp
@@ -7,61 +7,20 @@ short quguo[1001*1001]={0};
 int dx[] = {1, -1, 0, 0};
 int dy[] = {0, 0, 1, -1};
 int cnt=1;
-//void zou_migong(int x, int y, int n , int zou_gou) {
-//	// x,y: 起始点 , n: 迷宫的大小n*n, p:走过的标记
-//	for (int k = 0; k < 4; ++k) {
-//		int xx = x + dx[k];
-//		int yy = y + dy[k];
-//		if (xx >=0 && xx < n && yy >=0 && yy < n) {
-//			if((quguo[xx*n+yy]!=zou_gou) && (field[xx][yy]!=field[x][y])){
-//				cnt+=1;
-//				quguo[xx*n+yy]=zou_gou;
-//				zou_migong(xx, yy , n,zou_gou);
-//			}
-//		}
-//	}
-//	return;
-//}
-
 void zou_migong(int x, int y, int n , int zou_gou) {
-	// x,y: 起始点 , n: 迷宫的大小n*n, p:走过的标记
-	int xx, yy,zz;
-	char temp[4] = {0};
-  int flag = 1;
-
-  zou_gou = 1;
-  while (flag) {
-    
-    quguo[x*n+y] = zou_gou;
-    for (int i=-zou_gou;i<=zougou;i++) {
-      for (int j=-zou_gou;j<=zou_gou;j++){
-
-        xx = x +i;
-        yy = y +j;
-        zz = xx*n+yy;
-        if (zou_gou[zz] == zou_gou) {
-
-       }
-        xx = x + dx[0];
-        yy = y + dy[0];
-    		if (xx <0 || xx >= n || yy < 0 || yy >= n || quguo[zz]==zou_gou || field[xx][yy]==field[x][y]) continue;
+  // x,y: 起始点 , n: 迷宫的大小n*n, p:走过的标记
+  for (int k = 0; k < 4; ++k) {
+    int xx = x + dx[k];
+    int yy = y + dy[k];
+    if (xx >=0 && xx < n && yy >=0 && yy < n) {
+      if((quguo[xx*n+yy]!=zou_gou) && (field[xx][yy]!=field[x][y])){
+        cnt+=1;
+        quguo[xx*n+yy]=zou_gou;
+        zou_migong(xx, yy , n,zou_gou);
       }
-    
     }
-  
   }
-	for (int k = 0; k < 4; ++k) {
-		if (xx <0 || xx >= n || yy < 0 || yy >= n || quguo[zz]==zou_gou || field[xx][yy]==field[x][y]) continue;
-			
-		temp[k] = 1;
-		cnt+=1;
-		quguo[zz]=zou_gou;		
-	}
-	for (int k = 0; k < 4; ++k) {
-		if (temp[k] == 1)
-			zou_migong(x + dx[k], y + dy[k] , n,zou_gou);
-	}
-	return;
+  return;
 }
 int main() {
 
@@ -85,3 +44,76 @@ int main() {
 	}
 	return 0;
 }
+
+
+// #include <iostream> 
+// #include <queue>
+// using namespace std;
+// int m, n;
+
+// #define N 1002
+// char field[N][N]={0};
+// int quguo[N][N]={0};
+// int dx[] = {1, -1, 0, 0};
+// int dy[] = {0, 0, 1, -1};
+
+// typedef struct _Loc{
+//     int x;
+//     int y;
+//   }Loc;
+
+// int zou_migong_bfs(int x, int y, int n ,int zou_gou) {
+// 	// x,y: 起始点 , n: 迷宫的大小n*n
+// 	int xx, yy,xxx,yyy;
+//   int cnt = 1;
+//   
+//   queue<Loc> qu;
+
+//   Loc l; l.x = x; l.y = y; qu.push(l);
+//   quguo[x][y] = zou_gou; //qugio init with all 0
+
+//   while (!qu.empty()) {
+
+//     Loc l; l = qu.front();  xx=l.x;   yy=l.y; qu.pop();
+//     if(xx<1 || xx > n || yy < 1 || yy > n )  continue;
+
+//     for (int k = 0; k < 4; k++) {
+//       xxx = xx+dx[k]; yyy = yy+dy[k];
+//       if( quguo[xxx][yyy]!=zou_gou && field[xx][yy]!=field[xxx][yyy] && quguo[xxx][yyy]!= -1 ) {
+//         Loc l; l.x = xxx; l.y=yyy; qu.push(l);
+//         quguo[xxx][yyy] = zou_gou;
+//         cnt ++;
+//       }
+//     }
+
+//   }
+// 	return cnt;
+// }
+
+// int main() {
+// 	cin >> n >> m;
+// 	short max_zouguo[m];
+//   
+//   for(int i=0;i<n;i++)
+//         for(int j=0;j<n;j++)
+//             cin>>field[i+1][j+1];//读入地图 
+//                                  //
+//   for(int i=0;i<=n+1;i++){
+//     quguo[0][i] = -1;
+//     quguo[n+1][i] = -1;
+//     quguo[i][0] = -1;
+//     quguo[i][n+1] = -1;
+//   }
+
+// 	int x,y;
+// 	for(int k=0;k<m;k++){
+// 		cin>>x>>y;
+//     max_zouguo[k]=zou_migong_bfs(x,y,n,k+1);
+//     // cout << zou_migong_bfs(x,y,n,k+1) << endl;
+// 	}
+
+// 	for(int i=0;i<m;i++){
+// 		cout<<max_zouguo[i]<<endl;
+// 	}
+// 	return 0;
+// }
